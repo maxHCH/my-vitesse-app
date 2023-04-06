7a22476d207f8c31b959e5175b5ebf00194099c1
Max Hsieh <max.ghyun@gmail.com>
2023-04-06 20:12:30 +0800

    feat: init lite

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,31 +1,27 @@
-<script setup lang="ts">
+<script setup lang="ts" generic="T extends any, O extends any">
 defineOptions({
   name: 'IndexPage',
 })
-const user = useUserStore()
-const name = $ref(user.savedName)
+
+const name = $ref('')
 
 const router = useRouter()
-const go = () => {
+function go() {
   if (name)
     router.push(`/hi/${encodeURIComponent(name)}`)
 }
-
-const { t } = useI18n()
 </script>
 
 <template>
   <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
+    <div i-carbon-campsite text-4xl inline-block />
     <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
+      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
+        Vitesse Lite
       </a>
     </p>
     <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
+      <em text-sm op75>Opinionated Vite Starter Template</em>
     </p>
 
     <div py-4 />
@@ -36,21 +32,15 @@ const { t } = useI18n()
       autocomplete="false"
       @keydown.enter="go"
     />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
 
     <div>
       <button
-        btn m-3 text-sm
+        class="m-3 text-sm btn"
         :disabled="!name"
         @click="go"
       >
-        {{ t('button.go') }}
+        Go
       </button>
     </div>
   </div>
 </template>
-
-<route lang="yaml">
-meta:
-  layout: home
-</route>
